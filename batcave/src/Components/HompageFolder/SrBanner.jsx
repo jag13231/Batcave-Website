ae83736b2cc8e27f7a70b77165f03d9a6e730751
sridhar7358 <srisridhar8838@gmail.com>
2025-06-25 16:29:36 +0530

    edited banner section

--- batcave/src/Components/HompageFolder/SrBanner.jsx
+++ batcave/src/Components/HompageFolder/SrBanner.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import './SrBanner.css'
+
+
 
 const SrBanner = () => {
   return (
     <div>
-      <h1>Banner</h1>
+        <section className='banner'>
+            <h1>Welcome to Batcave</h1>
+            <p>“AN BATCAVE AUTOMOTIVE CLUB FOR PASSIONATE CAR ENTHUSIASTS<br/>THROUGH THRILLING EVENTS , EXCULSIVE CONTENT “</p>
+            <button>Join Community</button>        
+        </section>
+      
     </div>
   )
 }
