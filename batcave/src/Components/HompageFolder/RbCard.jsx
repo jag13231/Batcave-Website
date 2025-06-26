2b4e2f5ee4ac3fcaadf768aaeb3d3eef4f3f2c28
jag13231 <jagan112002@gmail.com>
2025-06-26 12:03:18 +0530

    review changes

--- batcave/src/Components/HompageFolder/RbCard.jsx
+++ batcave/src/Components/HompageFolder/RbCard.jsx
@@ -1,100 +1,36 @@
-import React from 'react'   
-import  './RbCard.css'
-import cardimg from '../../Assets/traffic-road-night[1].jpg'
-import cardimgtwo from '../../Assets/sport-cars-parade-race-highway[1].jpg'
-// import { MdCalendarToday } from 'react-icons/md';
-import calander from '../../Assets/Vector.png'
-import location from '../../Assets/carbon_location.png'
+import React from 'react'
+import TrafficImg from "../../Assets/traffic-road-night.jpg" 
+import "./RbCard.css"
 
-import cardimgthree from '../../Assets/joyful-fashionable-female-friends-raise-their-hands-while-sitting-luxury-cabriolet-car-park[1].jpg' 
-
-function RbCard() {
+const RbCard = () => {
   return (
-    // upcoming_events_container
-    <div  className='upcoming_events_container'>
-        <div className='card'>
-            <h5 className='heading_one'>UPCOMING </h5>
-            <p className='heading_two'>EVENTS</p>
+    <>
+    <div className="upcoming_events_container">
+        <div className="event_batch">
+            UPCOMING <span>EVENTS</span>
         </div>
+        <div className="cards_container">
+            <div className="event_cards">
+                <div className="event_img">
+                    <img src={TrafficImg} alt="" srcset="" />
+                </div>
+                
 
-        {/* registration_card */}
-        
-
-        <div className='registration_card'>
-
-            {/* registration_card1 */}
-            <div className='rb_card_container'>
-                <img className='rb_card_image' src={cardimg} alt='rb_card_image' />
-                <div className='rb_card_text'>
-                    <h3 className='card_heading'>Midnight Mile Rally</h3>
-                     <div className='card_icons'>
-                           <span> <img src={calander} alt="Calendar Icon" className='calendar_icon' /> 02 July , 2025</span>
-                        <span><img src={location} alt="Calendar Icon" className='calendar_icon' /> 10.00 PM</span>
-                        <span><img src={location} alt="Calendar Icon" className='calendar_icon' /> ECR  <br></br></span>
-                        </div>
-                <p className='card_text'>
-
-                    Join us for an unforgettable night as Batcave Automotive Club ignites the 
-                    highways under the stars. The Midnight Mile Rally is more than just a drive
-                     — it’s a pulse-pounding journey through moonlit roads.
-                </p>
-                {/* registration_button */}
-
-                <button className='rb_card_button'>Register Now</button>
+            </div>
+            <div className="event_cards">
+                 <div className="event_img">
+                    <img src={TrafficImg} alt="" srcset="" />
                 </div>
             </div>
-            
-            {/* registration_card1 */}
-
-
-           <div className='rb_card_container'>
-                <img className='rb_card_image' src={cardimgtwo} alt='rb_card_image' />
-                <div className='rb_card_text'>
-                    <h3 className='card_heading'>Midnight Mile Rally</h3>
-                     <div className='card_icons'>
-                           <span> <img src={calander} alt="Calendar Icon" className='calendar_icon' /> 02 July , 2025</span>
-                        <span><img src={location} alt="Calendar Icon" className='calendar_icon' /> 10.00 PM</span>
-                        <span><img src={location} alt="Calendar Icon" className='calendar_icon' /> ECR  <br></br></span>
-                        </div>
-                <p className='card_text'>
-
-                    Join us for an unforgettable night as Batcave Automotive Club ignites the 
-                    highways under the stars. The Midnight Mile Rally is more than just a drive
-                     — it’s a pulse-pounding journey through moonlit roads.
-                </p>
-
-                {/* registration_button */}
-
-                <button className='rb_card_button'>Register Now</button>
+            <div className="event_cards">
+                 <div className="event_img">
+                    <img src={TrafficImg} alt="" srcset="" />
                 </div>
             </div>
 
-
-
-
-           <div className='rb_card_container'>
-                <img className='rb_card_image' src={cardimgthree} alt='rb_card_image' />
-                <div className='rb_card_text'>
-                    <h3 className='card_heading'>Midnight Mile Rally</h3>
-                     <div className='card_icons'>
-                           <span> <img src={calander} alt="Calendar Icon" className='calendar_icon' /> 02 July , 2025</span>
-                        <span><img src={location}  alt="Calendar Icon" className='calendar_icon' /> 10.00 PM</span>
-                        <span><img src={location} alt="Calendar Icon" className='calendar_icon' /> ECR  <br></br></span>
-                        </div>
-                <p className='card_text'>
-
-                    Join us for an unforgettable night as Batcave Automotive Club ignites the 
-                    highways under the stars. The Midnight Mile Rally is more than just a drive
-                     — it’s a pulse-pounding journey through moonlit roads.
-                </p>
-
-                {/* registration_button */}
-
-                <button className='rb_card_button'>Register Now</button>
-                </div>
-            </div>
         </div>
     </div>
+    </>
   )
 }
 
