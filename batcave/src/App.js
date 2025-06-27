a4e6c52f90921a94b5ff47895e9d47103493e27a
jag13231 <jagan112002@gmail.com>
2025-06-27 13:23:05 +0530

    Some changes has been made

--- batcave/src/App.js
+++ batcave/src/App.js
@@ -1,5 +1,4 @@
 import HomeScreen from "./Pages/HomeScreen";
-
 import "./App.css";
 
 function App() {
--- batcave/src/Pages/HomeScreen.jsx
+++ batcave/src/Pages/HomeScreen.jsx
@@ -1,26 +1,22 @@
-import React from 'react'
-import Homepage from "../Components/HompageFolder/Header"
-import SrBanner from "../Components/HompageFolder/SrBanner"
-import RbCard from "../Components/HompageFolder/RbCard"
-import SrNews from '../Components/HompageFolder/SrNews'
-import SocialMedia from "../Components/HompageFolder/JgSocialMedia"
-import Footer from "../Components/HompageFolder/RbFooter"
-
+import React from "react";
+import Homepage from "../Components/HompageFolder/Header";
+import SrBanner from "../Components/HompageFolder/SrBanner";
+import RbCard from "../Components/HompageFolder/RbCard";
+import SrNews from "../Components/HompageFolder/SrNews";
+import SocialMedia from "../Components/HompageFolder/JgSocialMedia";
+import Footer from "../Components/HompageFolder/RbFooter";
 
 const HomeScreen = () => {
   return (
     <>
-    <Homepage />
-    <SrBanner/>
-    <RbCard />
-    <SrNews/>
-    <SocialMedia />
-    <Footer />
-
-    
-    
+      <Homepage />
+      <SrBanner />
+      <RbCard />
+      <SrNews />
+      <SocialMedia />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
