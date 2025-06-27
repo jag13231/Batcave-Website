0cf624e63a58a15b1d039cb5ed2f09b3abe9ccec
jag13231 <jagan112002@gmail.com>
2025-06-27 16:18:41 +0530

    Router Added and Event folder was created and Hompage is set to fixed in App.js

---
+++ batcave/src/Pages/EventScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EventBanner from '../Components/EventFolder/EventBanner'
+
+const EventScreen = () => {
+  return (
+    <>
+    <EventBanner />
+    </>
+  )
+}
+
+export default EventScreen
--- batcave/src/Pages/HomeScreen.jsx
+++ batcave/src/Pages/HomeScreen.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Homepage from "../Components/HompageFolder/Header";
 import SrBanner from "../Components/HompageFolder/SrBanner";
 import RbCard from "../Components/HompageFolder/RbCard";
 import SrNews from "../Components/HompageFolder/SrNews";
@@ -9,12 +8,12 @@ import Footer from "../Components/HompageFolder/RbFooter";
 const HomeScreen = () => {
   return (
     <>
-      <Homepage />
       <SrBanner />
       <RbCard />
       <SrNews />
       <SocialMedia />
       <Footer />
+      
     </>
   );
 };
