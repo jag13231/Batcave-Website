acfa450b8601e4595232b5fdf56cde5f08c297fa
jag13231 <jagan112002@gmail.com>
2025-06-30 23:36:08 +0530

    created  details folder

--- batcave/src/App.js
+++ batcave/src/App.js
@@ -1,7 +1,8 @@
 import Homepage from "./Components/HompageFolder/Header";
 import HomeScreen from "./Pages/HomeScreen";
-import EventScreen from "./Pages/EventScreen"
-import EventRegister from "./Pages/EventRegister"
+import EventScreen from "./Pages/EventScreen";
+import EventRegister from "./Pages/EventRegister";
+import EventDetails from "./Pages/EventDetailsPage"
 import {Route, Routes} from "react-router-dom";
 import "./App.css";
 
@@ -10,9 +11,10 @@ function App() {
     <>
     <Homepage />
       <Routes>
-        <Route path="/" element={<HomeScreen />} />
+        <Route path="/" element = {<HomeScreen />} />
         <Route path="/events" element={<EventScreen />} />
         <Route  path="/eventregister" element={<EventRegister />}/>
+        <Route path = "/eventdetails" element={<EventDetails />} /> 
       </Routes>
     </>
   );
---
+++ batcave/src/Pages/EventDetailsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EventDetails from "../Components/EventDetails/EventDetails"
+
+const EventDetailsPage = () => {
+  return (
+    <>
+    <EventDetails />
+    </>
+  )
+}
+
+export default EventDetailsPage
