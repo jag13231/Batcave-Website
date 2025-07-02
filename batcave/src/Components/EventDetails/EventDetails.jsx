4514347691ec0bc60130e176874e4d261112eb52
sridhar7358 <srisridhar8838@gmail.com>
2025-07-02 13:44:34 +0530

    add eventdetails

--- batcave/src/Components/EventDetails/EventDetails.jsx
+++ batcave/src/Components/EventDetails/EventDetails.jsx
@@ -3,12 +3,8 @@ import React from 'react'
 
 const EventDetails = () => {
   return (
-    <div className='event_details_container'>
-
-       <div><button className='backbutton'>Back</button></div>
-      
-dfdf
-    </div>
+    <>
+    </>
   )
 }
 
